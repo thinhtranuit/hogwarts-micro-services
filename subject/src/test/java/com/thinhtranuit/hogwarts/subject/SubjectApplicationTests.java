64fbe30180b3221424d6f5d62f52b7fb53359da3
Thomas Tran Van Thinh <thinhuit@gmail.com>
2024-03-16 10:44:28 +0700

    feat: init commit

---
+++ subject/src/test/java/com/thinhtranuit/hogwarts/subject/SubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinhtranuit.hogwarts.subject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
